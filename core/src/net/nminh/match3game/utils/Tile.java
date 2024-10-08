9c731c8b4d6775d3787da51af4d0c658c790c943
nminh123 <caonhatminh245@gmail.com>
2024-10-08 14:29:14 +0700

    Update Tile.java

--- core/src/net/nminh/match3game/utils/Tile.java
+++ core/src/net/nminh/match3game/utils/Tile.java
@@ -10,6 +10,8 @@ public class Tile extends Image
     public int col;
     public int type = -1;
 
+    public Tile(){}
+
     public Tile(int col, int row){
         this.col = col;
         this.row = row;
@@ -22,6 +24,14 @@ public class Tile extends Image
         setDrawable(new TextureRegionDrawable(sprite));
         debug();
         this.type = index;
+    }
+
+    public void init(TextureRegion sprite)
+    {
+        setSize(getWidth(), getHeight());
+        setBounds(getX(), getY(), getWidth(), getHeight());
+        setDrawable(new TextureRegionDrawable(sprite));
+        debug();
     }
 
     public void setRowCol(int row, int col){
